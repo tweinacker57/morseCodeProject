ed1ae025dcf0b254c517a0d9d55bc83a7349078d
Tyler Weinacker <tweinacker57@gmail.com>
2022-10-03 08:49:21 -0400

    Add message and secret message. Add dictionary of morse code values

---
+++ morseCode.swift
@@ -0,0 +1,35 @@
+//Create a morse code converter to decode seret messages
+
+var englishText = "this is a secret message"
+var secretMessage = ".... --- .-- -.. -.--   .--. .- .-. - -. . .-."
+
+//Create Morse Code Dictionary
+
+var lettersToMorse: [String: String] = [
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--.."
+]
